4d4d8e551e67f5cad2f6ff3879a6fbd4d714048e
Michał Kasperek <michal.kasperek2000@wp.pl>
2023-12-01 16:28:10 +0100

    Arrangement of buttons in the formm

--- Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Form1.cs
@@ -12,9 +12,42 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+
+        private Button[,] buttons;
+        private char currentPlayer = 'X';
         public Form1()
         {
             InitializeComponent();
+            InitializeBoard();
+        }
+        private void InitializeBoard()
+        {
+            buttons = new Button[3, 3];
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    buttons[row, col] = new Button();
+                    buttons[row, col].Width = 100;
+                    buttons[row, col].Height = 100;
+                    buttons[row, col].Top = row * 100;
+                    buttons[row, col].Left = col * 100;
+                    buttons[row, col].Tag = new Tuple<int, int>(row, col);
+                    buttons[row, col].Font = new System.Drawing.Font("Arial", 20F);
+                    buttons[row, col].Click += new EventHandler(Button_Click);
+
+                    Controls.Add(buttons[row, col]);
+                }
+            }
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
